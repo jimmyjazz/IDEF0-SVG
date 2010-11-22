57a6a92f0eb8828cb6178b147306f34dae8f519f
James Ross <james@switters.civica.com.au>
2010-11-22 15:43:11 +1100

    refactored to allow processes to know their position

--- spike.rb
+++ spike.rb
@@ -1,11 +1,31 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 module IDEF0
 
   class Process
 
+    attr_reader :x, :y
+
     def initialize(name)
       @name = name
+      @inputs = Set.new
+      @outputs = Set.new
+      @x = @y = 0
+    end
+
+    def receives(input)
+      @inputs << input
+    end
+
+    def produces(output)
+      @outputs << output
+    end
+
+    def move_to(x, y)
+      @x = x
+      @y = y
     end
 
     def width
@@ -16,7 +36,7 @@ module IDEF0
       60
     end
 
-    def to_svg(x, y)
+    def to_svg
       <<-XML
 <rect x='#{x}' y='#{y}' width='#{width}' height='#{height}' fill='none' stroke='black' />
 <text text-anchor='middle' x='#{x + (width / 2)}' y='#{y + (height / 2)}'>#{@name}</text>
@@ -32,7 +52,20 @@ XML
     end
 
     def process(name)
-      @processes << Process.new(name)
+      p = Process.new(name)
+      yield(p) if block_given?
+      @processes << p
+    end
+
+    def layout
+      x = 0
+      y = 0
+
+      @processes.each do |process|
+        process.move_to(x, y)
+        x += process.width + 20
+        y += process.height + 20
+      end
     end
 
     def to_svg
@@ -44,23 +77,21 @@ XML
 ]>
 <svg width='1024pt' height='768pt'>
   <g>
-    #{render_diagram_body}
+    #{generate_processes}
+    #{generate_lines}
   </g>
 </svg>
 XML
     end
 
-    def render_diagram_body
-      x = 0
-      y = 0
-
-      @processes.map do |process|
-        output = process.to_svg(x, y)
-        x += process.width + 20
-        y += process.height + 20
-        output
-      end.join("\n")
+    def generate_processes
+      @processes.map(&:to_svg).join("\n")
     end
+    
+    def generate_lines
+
+    end
+
   end
 
 end
@@ -70,5 +101,9 @@ d.process("Oversee Business Operations")
 d.process("Expand The Business")
 d.process("Manage Local Restaurant")
 d.process("Provide Supplies")
-d.process("Serve Customers")
+d.process("Serve Customers") do |process|
+  process.receives("Hungry Customer")
+  process.produces("Satisfied Customer")
+end
+d.layout
 puts d.to_svg
