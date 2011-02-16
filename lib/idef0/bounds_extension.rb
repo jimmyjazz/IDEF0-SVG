5405cf3db6237daa7e8e52eef188ee59edb51877
James Ross <james@cathandjames.com>
2011-02-16 17:26:00 +1100

    missing file

---
+++ lib/idef0/bounds_extension.rb
@@ -0,0 +1,29 @@
+module IDEF0
+
+  class BoundsExtension
+
+    attr_reader :north, :south, :east, :west
+
+    def initialize
+      @north = @south = @east = @west = 0
+    end
+
+    def north=(value)
+      @north = value if value > @north
+    end
+
+    def south=(value)
+      @south = value if value > @south
+    end
+
+    def east=(value)
+      @east = value if value > @east
+    end
+
+    def west=(value)
+      @west = value if value > @west
+    end
+
+  end
+
+end
