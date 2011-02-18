8415cfe780f6806e10697b12b1f50d6ad7c12ae5
Simon Harris <haruki_zaemon@mac.com>
2011-02-18 15:12:39 +1100

    Whoops!

--- lib/idef0/unsatisfied_output_line.rb
+++ lib/idef0/unsatisfied_output_line.rb
@@ -2,7 +2,7 @@ module IDEF0
 
   class UnsatisfiedOutputLine < ExternalOutputLine
 
-    def self.make_line(source, target)
+    def self.make_line(target, source)
       source.right_side.each_unattached_anchor do |anchor|
         target.right_side.expects(anchor.name)
         yield(new(source, target, anchor.name))
